9b259e90d1bfbe6136f7410e42af10f0a858d41b
Vasile <55320717+libercom@users.noreply.github.com>
2022-09-12 22:18:19 +0300

    Finished project init (#1)

---
+++ Kitchen/Services/KitchenService.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace Kitchen.Services
+{
+    public class KitchenService : BackgroundService
+    {
+        private readonly ILogger<KitchenService> _logger;
+        private readonly string _apiEndpoint;
+
+        public KitchenService(ILogger<KitchenService> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _apiEndpoint = configuration.GetSection("DinningHallEndpoint").Value;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var client = new HttpClient();
+
+                await Task.Delay(3000);
+
+                await client.PostAsync(_apiEndpoint, new StringContent("", Encoding.UTF8, "application/json"));
+            }
+        }
+    }
+}
